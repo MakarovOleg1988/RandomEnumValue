e9050565530173ed3f9e54269f1d8c72f4e5bb75
79264 <79264@DESKTOP-65729PJ>
2023-02-18 13:41:48 +0530

    Добавьте файлы проекта.

---
+++ RandomEnumValue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RandomEnumValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var value = RandomEnumValue<DirectionType>();
+                Console.WriteLine(value.ToString());
+            }
+        }
+
+        static Random _R = new Random();
+        static T RandomEnumValue<T>()
+        {
+            var v = Enum.GetValues(typeof(T));
+            return (T)v.GetValue(_R.Next(v.Length));
+        }
+    }
+
+    public enum DirectionType : int
+    {
+        Right,
+        Left,
+        Up,
+        Down
+    }
+}
